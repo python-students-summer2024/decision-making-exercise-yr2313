6399444a443ddd7197324e6792415c5a03d01504
bloombar <amos@cs.nyu.edu>
2023-11-01 04:13:01 -0400

    pylinted

--- main.py
+++ main.py
@@ -6,49 +6,51 @@ Feel free to comment out any of it in order to focus only on the functions you a
 
 from solutions import *
 
+
 def main():
-  # make use of the functions in solutions.py
+    # make use of the functions in solutions.py
 
-  # print out a friendly welcome
-  print("\n--- WELCOME! ---\n")
+    # print out a friendly welcome
+    print("\n--- WELCOME! ---\n")
 
-  # print out the current year
-  year = get_year()
-  print("\nThe current year is {}!".format(year))
+    # print out the current year
+    year = get_year()
+    print("\nThe current year is {}!".format(year))
 
-  # check whether a shape is square or not
-  print() # line break
-  if is_square():
-    print("Yes - this object is square!")
-  else:
-    print("Sorry... this object is not square.")
+    # check whether a shape is square or not
+    print()  # line break
+    if is_square():
+        print("Yes - this object is square!")
+    else:
+        print("Sorry... this object is not square.")
 
-  # print out the greatest of two numbers
-  print() # line break
-  greatest = get_greatest()
-  print("The largest of the two numbers is {}.".format(greatest))
-  
-  # print out a person's BMI statistical category
-  print() # line break!
-  bmi_category = get_bmi_category()
-  print("Your BMI stastical category is {}.".format(bmi_category))
+    # print out the greatest of two numbers
+    print()  # line break
+    greatest = get_greatest()
+    print("The largest of the two numbers is {}.".format(greatest))
 
-  # print out the total cost of a face masks purchase
-  print() # line break!
-  total = get_discount()
-  print("The total cost of your face masks is: {}.".format(total))
+    # print out a person's BMI statistical category
+    print()  # line break!
+    bmi_category = get_bmi_category()
+    print("Your BMI stastical category is {}.".format(bmi_category))
 
-  # print out whether it's currently a leap year
-  print()
-  this_year = get_year()
-  if is_leap_year():
-    print("{} is a leap year!!!!".format(this_year))
-  else:
-    print("Sorry.... {} is not a leap year.".format(this_year))
+    # print out the total cost of a face masks purchase
+    print()  # line break!
+    total = get_discount()
+    print("The total cost of your face masks is: {}.".format(total))
 
-  # print out a farewell message
-  print()
-  print("--- BYE BYE! ---")
+    # print out whether it's currently a leap year
+    print()
+    this_year = get_year()
+    if is_leap_year():
+        print("{} is a leap year!!!!".format(this_year))
+    else:
+        print("Sorry.... {} is not a leap year.".format(this_year))
+
+    # print out a farewell message
+    print()
+    print("--- BYE BYE! ---")
+
 
 # call the main function
 main()
--- solutions.py
+++ solutions.py
@@ -7,74 +7,81 @@ Your job is to complete the definitions of each function so that it achieves its
 import math
 import datetime
 
+
 def get_year():
-  """
-  This function is given to you... it is used by the is_leap_year() function below.
-  Do not modify this function.
-    :returns: The current year, e.g. 2020
-  """
-  now = datetime.datetime.now() # get the current time now
-  year = now.year  # the current year
-  return year
+    """
+    This function is given to you... it is used by the is_leap_year() function below.
+    Do not modify this function.
+      :returns: The current year, e.g. 2020
+    """
+    now = datetime.datetime.now()  # get the current time now
+    year = now.year  # the current year
+    return year
+
 
 def is_square():
-  """
-  Asks the user to enter the width and height of an area in inches and determines whether it is square.
-  Users can enter either integers or floating point numbers.
+    """
+    Asks the user to enter the width and height of an area in inches and determines whether it is square.
+    Users can enter either integers or floating point numbers.
 
-    :returns: True if square (i.e. if equal length and height), False otherwise.
-  """
-  #### write your solution for this function below here. ####
-  
+      :returns: True if square (i.e. if equal length and height), False otherwise.
+    """
+    #### write your solution for this function below here. ####
+
+
 def get_greatest():
-  """
-  Asks the user for two integers, and returns the number that is greatest, as an int.
-  You are not allowed to use Python's builtin max() function for this.
-  If both numbers are the same, return that number.
+    """
+    Asks the user for two integers, and returns the number that is greatest, as an int.
+    You are not allowed to use Python's builtin max() function for this.
+    If both numbers are the same, return that number.
 
-  :returns: the greatest of the two input numbers, as an int.
-  """
-  #### write your solution for this function below here. ####
+    :returns: the greatest of the two input numbers, as an int.
+    """
+    #### write your solution for this function below here. ####
+
 
 def get_bmi_category():
-  """
-  Asks the user to enter their height (in inches) and weight (in pounds), in that order, and then returns the user's BMI statistical category.
-  Users can enter either integers or floating point numbers.
+    """
+    Asks the user to enter their height (in inches) and weight (in pounds), in that order, and then returns the user's BMI statistical category.
+    Users can enter either integers or floating point numbers.
 
-  The BMI formula was developed in the 1830s, and is still widely used today in public health policy, medical practice, and legislation.
-  The formula for calculating BMI is 703 * weight / height^2.
-  The BMI statistical categories are: 
-  - Very severely underweight (BMI < 15)
-  - Severely underweight (15 <= BMI < 16)
-  - Underweight (16 <= BMI < 18.5)
-  - Normal (18.5 <= BMI < 25)
-  - Overweight (25 <= BMI < 30)
-  - Moderately obese (30 <= BMI < 35)
-  - Severely obese (35 <= BMI < 40)
-  - Very severely obese (BMI >= 40)
+    The BMI formula was developed in the 1830s, and is still widely used today in public health policy, medical practice, and legislation.
+    The formula for calculating BMI is 703 * weight / height^2.
+    The BMI statistical categories are:
+    - Very severely underweight (BMI < 15)
+    - Severely underweight (15 <= BMI < 16)
+    - Underweight (16 <= BMI < 18.5)
+    - Normal (18.5 <= BMI < 25)
+    - Overweight (25 <= BMI < 30)
+    - Moderately obese (30 <= BMI < 35)
+    - Severely obese (35 <= BMI < 40)
+    - Very severely obese (BMI >= 40)
 
-    :returns: The name of the BMI statistical category, based on the inputted height and weight.
-  """
-  #### write your solution for this function below here. ####
+      :returns: The name of the BMI statistical category, based on the inputted height and weight.
+    """
+    #### write your solution for this function below here. ####
+
 
 def get_discount():
-  """
-  Imagine this scenario: a surgical mask distributor will give you a 20% discount on orders of 5000 or more.
-  Each mask individually costs $5.
-  This function asks the user how many masks they would like, and returns the total cost after applying any relevant discount
-  The total cost must be rounded to the nearest integer and formatted as in "$4,000".
+    """
+    Imagine this scenario: a surgical mask distributor will give you a 20% discount on orders of 5000 or more.
+    Each mask individually costs $5.
+    This function asks the user how many masks they would like, and returns the total cost after applying any relevant discount
+    The total cost must be rounded to the nearest integer and formatted as in "$4,000".
 
-    :returns: The cost of the masks, after any discounts, e.g. "$4,000" for 1000 masks.
-  """
-  #### write your solution for this function below here. ####
+      :returns: The cost of the masks, after any discounts, e.g. "$4,000" for 1000 masks.
+    """
+    #### write your solution for this function below here. ####
+
 
 def is_leap_year():
-  """
-  Determines whether the current year is a leap year.
-  Any year evenly divisible by 4 is a leap year, except century years such as 1900, 2000, 2100, etc. Only century years evenly divisible by 400 are leap years.
+    """
+    Determines whether the current year is a leap year.
+    Any year evenly divisible by 4 is a leap year, except century years such as 1900, 2000, 2100, etc. Only century years evenly divisible by 400 are leap years.
 
-  :returns: True if the current year is a leap year, False otherwise.
-  """
-  year = get_year() # this line is given to you - the variable, year, holds the current year
-  #### write your solution for this function below here. ####
-
+    :returns: True if the current year is a leap year, False otherwise.
+    """
+    year = (
+        get_year()
+    )  # this line is given to you - the variable, year, holds the current year
+    #### write your solution for this function below here. ####
